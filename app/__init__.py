1233ffaf370de71e317f048ed2ca798e7b292d15
Zygimantass <zygimantelis@gmail.com>
2019-11-16 13:06:50 +0200

    added api key endpoint

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask
+from flask_influxdb import InfluxDB
+
+import api.keys.controller
+
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY', "NOT_DEFINED")
+
+if GOOGLE_API_KEY == "NOT_DEFINED":
+    print("Please put your Google API key in the environment variables!")
+    quit()
+
+db = InfluxDB()
+
+def create_app():
+    app = Flask(__name__)
+    db.init_app(app)
+
+    app.config['GOOGLE_API_KEY'] = GOOGLE_API_KEY
+
+    with app.app_context():
+        app.register_blueprint(api.keys.controller.keys, url_prefix='/api/keys')
+
+    return app
