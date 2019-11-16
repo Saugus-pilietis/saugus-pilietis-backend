edab7156ad38c12fba971170c3b4e50470579346
Zygimantass <zygimantelis@gmail.com>
2019-11-16 15:55:50 +0200

    added configuration for influx

--- app/__init__.py
+++ app/__init__.py
@@ -10,13 +10,25 @@ if GOOGLE_API_KEY == "NOT_DEFINED":
     print("Please put your Google API key in the environment variables!")
     quit()
 
+
+
 db = InfluxDB()
+
+def initialize_config_from_env(app):
+    app.config["INFLUXDB_HOST"] = os.getenv("INFLUXDB_HOST")
+    app.config["INFLUXDB_PORT"] = os.getenv("INFLUXDB_PORT")
+    app.config["INFLUXDB_USER"] = os.getenv("INFLUXDB_USER")
+    app.config["INFLUXDB_PASS"] = os.getenv("INFLUXDB_PASS")
+    
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+    
+    app.config["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
 
 def create_app():
     app = Flask(__name__)
     db.init_app(app)
 
-    app.config['GOOGLE_API_KEY'] = GOOGLE_API_KEY
+    initialize_config_from_env(app)
 
     with app.app_context():
         app.register_blueprint(api.keys.controller.keys, url_prefix='/api/keys')
